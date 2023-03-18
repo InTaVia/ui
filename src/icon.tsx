1940e7a8cca4d16ea480f9bba04f6a04d6e0e2b0
Stefan Probst <stefanprobst@posteo.de>
2023-03-18 20:10:54 +0100

    chore: wip

---
+++ src/icon.tsx
@@ -0,0 +1,33 @@
+import { isNonEmptyString } from "@stefanprobst/is-nonempty-string";
+import { type ForwardedRef, type ReactNode, type SVGProps, forwardRef } from "react";
+
+interface IconProps<T extends string> extends SVGProps<SVGSVGElement> {
+	children?: ReactNode;
+	icon: T;
+}
+
+export const Icon = forwardRef(function Icon<T extends string>(
+	props: IconProps<T>,
+	forwardedRef?: ForwardedRef<SVGSVGElement>,
+): JSX.Element {
+	const { children, icon, ...rest } = props;
+
+	const href = "#" + icon;
+
+	return (
+		<svg
+			ref={forwardedRef}
+			aria-hidden={isNonEmptyString(props["aria-label"]) === false}
+			fill="currentColor"
+			stroke="currentColor"
+			width="1em"
+			{...rest}
+		>
+			<use xlinkHref={href} />
+			{children}
+		</svg>
+	);
+}) as <T extends string>(
+	props: IconProps<T>,
+	forwardedRef?: ForwardedRef<SVGSVGElement>,
+) => JSX.Element;
--- src/icons.tsx
+++ src/icons.tsx
@@ -1,31 +1,21 @@
-import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from "react";
+import type { ReactNode } from "react";
 
-type IconProps = ComponentPropsWithoutRef<"svg">;
-type IconElement = ElementRef<"svg">;
+const symbols = {
+	spinner: <path d="M21 12a9 9 0 1 1-6.219-8.56" />,
+} satisfies Record<string, ReactNode>;
 
-export const SpinnerIcon = forwardRef<IconElement, IconProps>(function SpinnerIcon(
-	props,
-	forwardedRef,
-): JSX.Element {
-	const { children, ...rest } = props;
-
+export function Icons(): JSX.Element {
 	return (
-		<svg
-			ref={forwardedRef}
-			width={24}
-			height={24}
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			strokeWidth={2}
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			{...rest}
-		>
-			<path d="M21 12a9 9 0 1 1-6.219-8.56" />
-			{children}
+		<svg style={{ display: "none" }}>
+			<defs>
+				{Object.entries(symbols).map(([id, symbol]) => {
+					return (
+						<symbol key={id} id={id}>
+							{symbol}
+						</symbol>
+					);
+				})}
+			</defs>
 		</svg>
 	);
-});
-
-SpinnerIcon.displayName = "SpinnerIcon";
+}
